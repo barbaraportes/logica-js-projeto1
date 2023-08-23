2773bd5898d79e6d1448a1ad7812e616c60b9d46
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-23 18:08:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-alert('Boas vindas ao jogo do número secreto!')
+alert('Boas vindas ao jogo do número secreto!');
 
-let numeroSecreto = 15
-console.log(`O número secreto é ${numeroSecreto}`)
+let numeroSecreto = 15;
+console.log(`O número secreto é ${numeroSecreto}`);
 
-let chute = prompt('Escolha um número entre 1 e 30')
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto == chute) {
-    alert(`Você descobriu o número secreto ${numeroSecreto}!`)
-} else {
-    alert('Você errou! :(')
+//enquanto chute não for igual ao número secreto...
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+
+    if (chute == numeroSecreto) {
+        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+    } else {
+        if (chute < numeroSecreto) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+
+        tentativas ++
+       
+    }
 }
+
+
 
