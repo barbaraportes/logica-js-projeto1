61788d4b386bd39065cccd1694c0a62da91923a1
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-23 18:43:13 -0300

    Update ambiente.js

--- ambiente.js
+++ ambiente.js
@@ -1,8 +1,30 @@
 //exercicios
 
+/*
 let cont = 1;
-
 while (cont <= 10) {
     console.log(`Número ${cont}º`)
     cont++
 }
+*/
+
+/*
+let cont = 10;
+
+while (cont >= 1) {
+    console.log(`Número ${cont}º`)
+    cont--
+}
+*/
+
+
+/*
+let num = prompt('Quer contar até que número?');
+let cont = 0;
+while (cont <= num) {    
+    alert(cont)
+    cont++
+} 
+*/
+
+
