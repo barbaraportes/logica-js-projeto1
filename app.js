a3e938712737fe6d39cdcf110c237c8d32c66750
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-28 22:50:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 alert('Boas vindas ao jogo do número secreto!');
 
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 10;
+
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
 
 /*
     parseInt pega apenas a parte inteira do valor
@@ -9,14 +12,14 @@ let numeroSecreto = parseInt(Math.random() * 100 + 1);
     + 1 para chegar ao 10 e ao 100 e etc
 */
 
-console.log(`O número secreto é ${numeroSecreto}`);
+
 
 let chute;
 let tentativas = 1;
 
 //enquanto chute não for igual ao número secreto...
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (chute == numeroSecreto) {
         break;
