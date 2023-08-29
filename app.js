8a8c5e9114ca9aecda3dbd06677b64b6b0a98b29
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-28 22:41:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
 
-let numeroSecreto = 15;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+
+/*
+    parseInt pega apenas a parte inteira do valor
+    Math.random() gera um número pseudo aleatório entre 0 e 1
+    * 100 para ter 3 digitos
+    + 1 para chegar ao 10 e ao 100 e etc
+*/
+
 console.log(`O número secreto é ${numeroSecreto}`);
 
 let chute;
@@ -8,10 +16,10 @@ let tentativas = 1;
 
 //enquanto chute não for igual ao número secreto...
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 30');
+    chute = prompt('Escolha um número entre 1 e 100');
 
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        break;
     } else {
         if (chute < numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
@@ -24,5 +32,17 @@ while (chute != numeroSecreto) {
     }
 }
 
+//operador ternário
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
 
+/* 
+      - O código abaixo tem o mesmo resultado que o código acima (operador ternário) -
+
+if (tentativas > 1) {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+} else {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+}
+*/
